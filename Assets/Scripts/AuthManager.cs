4d6f2e5deb39e3ffe8f757c882c16ff17c70ec9c
ismailonur <ismailonur15@gmail.com>
2020-10-15 16:39:48 +0300

    mail ile kayıt olma ve anonim giriş yapma kodları yazıldı

---
+++ Assets/Scripts/AuthManager.cs
@@ -0,0 +1,65 @@
+﻿using Firebase.Auth;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AuthManager : MonoBehaviour
+{
+    public FirebaseAuth auth;
+
+    public InputField emailForm;
+    public InputField passwordForm;
+
+    void Start()
+    {
+        auth = FirebaseAuth.DefaultInstance;
+
+        AnonymousSignUp();
+    }
+
+    public void SignUp()
+    {
+        string email = emailForm.text;
+        string password = passwordForm.text;
+
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.Log("Canceled");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.Log("Faulted");
+                return;
+            }
+
+            // Başarılı işlem
+            FirebaseUser newUser = task.Result;
+            Debug.LogFormat("Yeni kullanıcı oluşturuldu: {0} {1}", newUser.DisplayName, newUser.UserId);
+        });
+    }
+
+    public void AnonymousSignUp()
+    {
+        auth.SignInAnonymouslyAsync().ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.Log("Canceled");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.Log("Faulted");
+                return;
+            }
+
+            FirebaseUser newUser = task.Result;
+            Debug.LogFormat("Anonim kullanıcı oluşturuldu {0} {1}", newUser.DisplayName, newUser.UserId);
+        });
+    }
+
+}
