5c49e957bc27aa45fdb5868a1db593f3bad23e11
ismailonur <ismailonur15@gmail.com>
2020-10-16 11:36:54 +0300

    Database bağlantısının kurulumu için scirpt yazıldı

---
+++ Assets/Scripts/DBManager.cs
@@ -0,0 +1,29 @@
+﻿using Firebase;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DBManager : MonoBehaviour
+{
+    void Start()
+    {
+        Initialization();
+    }
+
+    void Initialization()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Bağlantı kurulduktan sonra gerekli işlemler
+                Debug.Log("DB Bağlantısı Kuruldu.");
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format("Hata {0}", dependencyStatus));
+            }
+        });
+    }
+}
